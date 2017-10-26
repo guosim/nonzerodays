17070344a6fab24345b5980ccb46268c42f92be2
Henry Wu <guosims@yahoo.com>
2017-10-25 21:19:39 -0700

    login and logout functions

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { loginEmail } from '../firebase.js';
 import './LoginSignup.css';
 
 class Login extends React.Component {
+	constructor() {
+		super();
+		this.login = this.login.bind(this);
+	}
+
+	login(event) {
+		event.preventDefault();
+		loginEmail(this.email.value, this.password.value)
+		this.password.value = '';
+	}
 
 	render() {
 		return (
 			<div className="loginsignup-flex">
-				<form className="loginsignup">
+				<form ref={(input) => {this.loginForm = input}} className="loginsignup" onSubmit={(e) => this.login(e)}>
 					<h3 className="loginsignup-header">Log In</h3>
-					<input ref={(input) => {this.username = input}} className="loginsignup-field" type="text" placeholder="username or email" required autoFocus={true} />
-					<input ref={(input) => {this.password = input}} className="loginsignup-field" type="text" placeholder="password" required />
+					<input ref={(input) => {this.email = input}} className="loginsignup-field" type="text" placeholder="username or email" required autoFocus={true} />
+					<input ref={(input) => {this.password = input}} className="loginsignup-field" type="password" placeholder="password" required />
 					<button type="submit" className="loginsignup-submit">Log In</button>
 					<Link to="/signup" className="loginsignup-link">Don't have an account? Sign up here!</Link>
 				</form>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,32 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { createUser } from '../firebase.js';
 import './LoginSignup.css';
-import firebase from '../firebase.js';
+
 
 class Signup extends React.Component {
 	constructor() {
 		super();
 		this.makeUser = this.makeUser.bind(this);
-		this.state = {
-			error: ""
-		};
 	}
 
 	makeUser(event) {
 		event.preventDefault();
-		firebase
-			.auth()
-			.createUserWithEmailAndPassword(this.email.value, this.password.value)
-			.catch(function(error) {
-				console.log(error.message);
-			});
+		createUser(this.email.value, this.password.value)
 		this.password.value = '';
 		this.verify.value = '';
-
-		firebase.auth().onAuthStateChanged(function(user) {
-			window.user = user;
-			console.log(window.user);
-		});
 	}
 
 	render() {
@@ -34,7 +22,7 @@ class Signup extends React.Component {
 			<div className="loginsignup-flex">
 				<form ref={(input) => {this.signupForm = input}} className="loginsignup" onSubmit={(e) => this.makeUser(e)}>
 					<h3 className="loginsignup-header">Create A New Account</h3>
-					<p className="errorMessage"></p> 
+					<p className="errorMessage">{this.msg}</p> 
 					<input ref={(input) => {this.username = input}} className="loginsignup-field" type="text" placeholder="username" required autoFocus={true} />
 					<input ref={(input) => {this.password = input}} className="loginsignup-field" type="password" placeholder="password" required />
 					<input ref={(input) => {this.verify = input}} className="loginsignup-field" type="password" placeholder="verify password" required />

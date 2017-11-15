a39294dff5f6186940a76dfeb7d30cd9f6db9794
Henry Wu <guosims@yahoo.com>
2017-11-15 01:24:46 -0800

    update readme

---
+++ README.md
@@ -0,0 +1,6 @@
+To Run Locally
+======
+1. git clone https://github.com/guosim/nonzerodays.git
+2. cd nonzerodays
+3. npm install
+4. npm start
--- src/components/Task.js
+++ src/components/Task.js
@@ -35,51 +35,26 @@ class Task extends React.Component {
 	render() {
 		const { details, index } = this.props;
 		const complete = formatTimeAgo(this.state.current_time - details.complete[details.complete.length - 1]) || "Incomplete";
-		const disabled = isComplete ? true : false;
-
-
-		let modalStyle = {
-			position:'absolute',
-			top:'50%',
-			left:'50%',
-			transform:'translate(-50%, -50%)',
-			zIndex:'999',
-			background:'#fff'
-		}
-
-		let backdropStyle = {
-			position: 'absolute',
-			width:'100%',
-			height:'100%',
-			top:'0px',
-			left:'0px',
-			zIndex:'998',
-			background:'rgba(0,0,0,.3)'
-		}
-		
-			return (
-				<li className="task">
-					<i className="fa fa-pencil edit"></i>
-					<h3 className="task-name">
-						{details.name}
-					</h3>
-					<ReactStars
-						className="taskDifficulty"
-						count={5}
-						value={details.difficulty}
-						size={24}
-						half={false}
-						color2={'#ffd700'} 
-						edit={false} />
-					<p className="taskDescription">
-						{details.description}
-					</p>
-					<button onClick={() => this.props.completeTask(index)} className="complete-task" disabled={ disabled } >{ complete }</button>
-				</li>
-			)
-	
-
-	}
+		return (
+			<li className="task">
+				<i className="fa fa-pencil edit"></i>
+				<h3 className="task-name">
+					{details.name}
+				</h3>
+				<ReactStars
+					className="taskDifficulty"
+					count={5}
+					value={details.difficulty}
+					size={24}
+					half={false}
+					color2={'#ffd700'} 
+					edit={false} />
+				<p className="taskDescription">
+					{details.description}
+				</p>
+				<button onClick={() => this.props.completeTask(index)} className="complete-task">{ complete }</button>
+			</li>
+		)}
 }
 
 Task.propTypes = {
